1ecb1e3300e9a4e6dcc7c1fb9ee68aa3483baf7c
Rafael Malespin <leoblade24@ufl.edu>
2019-10-31 15:16:04 -0400

    added Contact and Proof Of Concept components

---
+++ client/src/components/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function ContactUs(){
+    return(
+        <div>
+            <h1>Contact Us </h1>
+            <p>
+                This is a placeholder for the information for the employees sections and
+                specific employees contact information
+            </p>
+        </div>
+    );
+}
