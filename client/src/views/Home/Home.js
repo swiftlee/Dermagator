e8f26acc3056abf403a746c9d4056d3aed535649
Rafael Malespin <leoblade24@ufl.edu>
2019-11-04 10:17:17 -0500

    Added a ProductInfo Section that needs expanding for final product

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import ProofOfConcepts from "../../components/ProofOfConcepts";
 import ContactUs from "../../components/ContactUs";
 import AboutUs from "../../components/AboutUs";
+import ProductInfo from "../../components/Products"
 
 function Home() {
     return (
@@ -10,6 +11,7 @@ function Home() {
             <AboutUs/>
             <ProofOfConcepts/>
             <ContactUs/>
+            <ProductInfo/>
         </div>
     );
 }
