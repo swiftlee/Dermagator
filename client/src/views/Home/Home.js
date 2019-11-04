4ec326501834032d36066aba06a7ddccfd51787d
Jonathan Conlin <jconlin@ufl.edu>
2019-11-04 12:23:12 -0500

    Update Home.js

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,21 +3,12 @@ import './Home.css';
 import ProofOfConcepts from "../../components/ProofOfConcepts";
 import ContactUs from "../../components/ContactUs";
 import AboutUs from "../../components/AboutUs";
-<<<<<<< HEAD
 import ProductInfo from "../../components/Products"
-=======
 import NavBar from "../../components/Header/NavBar";
->>>>>>> 6aae3957a07ad40844beb6835146c16f0f751269
 
 function Home() {
     return (
         <div className="App">
-<<<<<<< HEAD
-            <AboutUs/>
-            <ProofOfConcepts/>
-            <ContactUs/>
-            <ProductInfo/>
-=======
             <div className='mb-5 sticky-top'>
                 <NavBar/>
             </div>
@@ -27,10 +18,12 @@ function Home() {
             <div id='proof-of-concept' className='mb-5'>
                 <ProofOfConcepts/>
             </div>
+            <div id='product' className='mb-5'>
+                <ProductInfo/>
+            </div>
             <div id='contact' style={{marginTop:"50rem"}}> {/*REMOVE STYLE WHEN DONE TESTING*/}
                 <ContactUs/>
             </div>
->>>>>>> 6aae3957a07ad40844beb6835146c16f0f751269
         </div>
     );
 }
