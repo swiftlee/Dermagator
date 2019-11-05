8def192bc216290a740cc08953886088bb0a838f
Rafael Malespin <leoblade24@ufl.edu>
2019-11-05 08:48:55 -0500

    Temp

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,21 +3,11 @@ import './Home.css';
 import ProofOfConcepts from "../../components/ProofOfConcepts";
 import ContactUs from "../../components/ContactUs";
 import AboutUs from "../../components/AboutUs";
-<<<<<<< HEAD
-import ProductInfo from "../../components/Products"
-=======
 import NavBar from "../../components/Header/NavBar";
->>>>>>> 6aae3957a07ad40844beb6835146c16f0f751269
 
 function Home() {
     return (
         <div className="App">
-<<<<<<< HEAD
-            <AboutUs/>
-            <ProofOfConcepts/>
-            <ContactUs/>
-            <ProductInfo/>
-=======
             <div className='mb-5 sticky-top'>
                 <NavBar/>
             </div>
@@ -30,7 +20,6 @@ function Home() {
             <div id='contact' style={{marginTop:"50rem"}}> {/*REMOVE STYLE WHEN DONE TESTING*/}
                 <ContactUs/>
             </div>
->>>>>>> 6aae3957a07ad40844beb6835146c16f0f751269
         </div>
     );
 }
