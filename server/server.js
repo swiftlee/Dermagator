9a4268f42ec484ae8a18b843ae39ecaa31a28bc6
Jon <jconlin@ufl.edu>
2019-11-12 11:31:08 -0500

    stripped backend of templating

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const express = require('./config/express.js')
- 
+import express from 'express';
+import path from 'path';
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 // Use env port or default
 const port = process.env.PORT || 5000;
-
-const app = express.init()
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
