fede04ee80248d7cced6428189142910035b847a
jjmorales <jonjoemorales@gmail.com>
2019-11-11 23:15:05 -0500

    removed background card for team section alignment with wireframe

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -31,12 +31,12 @@ const About=()=>{
                     Placeholder for information regarding partners for DermaGator.
                 </p>
                 </div>
-                <div className='row mb-5' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
+                <div className='mb-5-nobox' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
                  data-aos-mirror="true">
                 <h1>TEAM TEXT</h1>
                 <div style={{display: 'flex', flexDirection: 'row'}} className = "text-center">
-                <InfoCard title='Guy 1' subtitle='' info='info1' data-aos-offset='0' style={{flex: 1}}/>
-                <InfoCard title='Guy 2' subtitle='' info='info2' data-aos-offset='0' style={{flex: 1}}/>
+                <InfoCard title='Guy 1' subtitle='' info='info1' data-aos-offset='0' style={{flex: 1}} className='row mb-5 '/>
+                <InfoCard title='Guy 2' subtitle='' info='info2' data-aos-offset='0' style={{flex: 1}} className='row mb-5'/>
                 </div>
                 </div>
         </div>
