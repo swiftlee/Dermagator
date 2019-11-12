c012514082acbd0019980546b195dae290412169
Jon <jconlin@ufl.edu>
2019-11-12 12:27:55 -0500

    updated ts again

--- server/dist/server.js
+++ server/dist/server.js
@@ -2,22 +2,20 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path = __importStar(require("path"));
+const path_1 = __importDefault(require("path"));
+const dotenv_1 = require("dotenv");
+const result = dotenv_1.config();
 const app = express_1.default();
-app.use(express_1.default.static(path.join(__dirname, 'client/build')));
+if (result.error)
+    throw result.error;
+app.use(express_1.default.static(path_1.default.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path_1.default.join(__dirname + '/client/build/index.html'));
 });
 // Use env port or default
+console.log(`THE PORT IS THIS THING: ${process.env.PORT}`);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
 //# sourceMappingURL=server.js.map
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-import * as path from 'path';
+import path from 'path';
+import {config} from "dotenv";
+
+const result = config();
 const app = express();
+
+if (result.error)
+    throw result.error;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
@@ -8,5 +14,6 @@ app.get('*', (req, res) => {
 });
 
 // Use env port or default
+console.log(`THE PORT IS THIS THING: ${process.env.PORT}`);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
