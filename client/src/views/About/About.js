1936fedb1db17fe07880b5197d2d161bf3dc322f
jjmorales <jonjoemorales@gmail.com>
2019-11-11 22:43:09 -0500

    about-us section formatting

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import './About.css';
 import NavBar from '../../components/Header/NavBar';
+
 const About=()=>{
     return(
-        <div className="text-center m-auto">
+        <div>
             <div className='sticky-top'>
                 <NavBar/>
             </div>
-
-            <div className="sectionTitle">
+                <div className='row mb-5' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
+                 data-aos-mirror="true">
                 <h1>MISSION TEXT</h1>
                 <p>
                     Placeholder for information regarding mission for DermaGator.
                 </p>
-            </div>
-            <div>
+                </div>
+                <div className='row mb-5' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
+                 data-aos-mirror="true">
                 <h1>OUR STORY TEXT</h1>
                 <p>
                     Placeholder for information regarding story for DermaGator.
                 </p>
-            </div>
-            <div>
+                </div>
+                <div className='row mb-5' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
+                 data-aos-mirror="true">
                 <h1>PARTNERS TEXT</h1>
                 <p>
                     Placeholder for information regarding partners for DermaGator.
                 </p>
-            </div>
-            <div>
+                </div>
+                <div className='row mb-5' data-aos="fade" data-aos-duration='1000' data-aos-easing="ease-in-out"
+                 data-aos-mirror="true">
                 <h1>TEAM TEXT</h1>
-                <p>
-                    Placeholder for information regarding the team for DermaGator.
-                </p>
-            </div>
-
+                </div>
         </div>
     );
 
