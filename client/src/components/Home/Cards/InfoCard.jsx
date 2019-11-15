88c1ccbf931ab98f1a960c36a5590e56bf772307
Nicholas Alvarez <nick12al@gmail.com>
2019-11-14 19:15:46 -0500

    card color

--- client/src/components/Home/Cards/InfoCard.jsx
+++ client/src/components/Home/Cards/InfoCard.jsx
@@ -5,7 +5,7 @@ const InfoCard = ({title, subtitle, info}) => {
     return (
         <div className='ml-5' style={{width:'30%'}}>
             <div className="card border-0 shadow rounded">
-                <h5 className="card-header">{title}</h5>
+                <h5 className="card-header" style = {{backgroundColor: "#4484ce"}}>{title}</h5>
                 <div className="card-body">
                     <h5 className="card-subtitle">{subtitle}</h5>
                     <p className="card-text">{info}</p>
