0cd08b9dad891d823467009046ba714f0ce63f0f
Jon <horizonman34@gmail.com>
2019-11-14 19:24:58 -0500

    merged origin/develop into develop

--- server/dist/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    db: {
-        uri: 'mongodb://127.0.0.1:27017/dermagator',
-    }
-};
-//# sourceMappingURL=config.js.map
