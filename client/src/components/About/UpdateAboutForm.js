46af34f86ac87cf6364a3b11651bafa09d2e5003
Rafael Malespin <leoblade24@ufl.edu>
2019-11-29 16:09:22 -0500

    Updated forms and added them to the new Dashboard; we need to set up the menu for actually selecting what appears

--- client/src/components/About/UpdateAboutForm.js
+++ client/src/components/About/UpdateAboutForm.js
@@ -1,86 +1,89 @@
 import React,{useState} from 'react';
 import useAboutPage from '../../views/AdminDashboard/useAboutPage';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const AboutUpdate=()=>{
     let {inputs,handleSubmit,setText,setName,setInfoCardName,setInfoCardPerson,setInfoCardText,handleNewSection,
         handleNewSectionChange,section,handleEmployeeChange,handleNewEmployee,employee,deleteSection,deleteEmployee}=useAboutPage();
     return(
         <div>
+            <h2>Update or Delete Sections on About Page</h2>
             {
                 inputs.data.map((stuff,index)=>{
                     if(stuff.type==="section"){
                         return(
-                            <form>
-                                <h4>Title of Section {index+1}:</h4>
-                                <input type="text"
+                            <Form>
+                                <Form.Label>Title of Section {index+1}:</Form.Label>
+                                <Form.Control type="text"
                                 placeholder="Name to Update"
                                 value={inputs.data[index].name}
                                 onChange={event=>setName(index,event.target.value)}
                                 required/>
-                                <h4>Text of Section {index+1}</h4>
-                                <textarea type="text"
+                                <Form.Label>Text of Section {index+1}</Form.Label>
+                                <Form.Control as="textarea" rows="5" type="text"
                                 placeholder="Text to Update"
                                 value={inputs.data[index].text}
                                 onChange={event=>setText(index,event.target.value)}
                                 required/>
-                                <button type="submit" onClick={handleSubmit}> Update Section {index+1}</button>
-                                <button type="submit" onClick={event=>deleteSection(index,event)}>Delete Section {index+1}</button>
-                            </form>
+                                <Button type="submit" onClick={handleSubmit}> Update Section {index+1}</Button>
+                                <Button type="submit" onClick={event=>deleteSection(index,event)}>Delete Section {index+1}</Button>
+                            </Form>
                         )
                     }
                     else{
                         return(
                             <div>
-                                <form onSubmit={handleSubmit}>
-                                    <h4>Title of Section {index+1}:</h4>
-                                    <input type="text"
+                                <h2>Update or Delete Employees</h2>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Label>Title of Section {index+1}:</Form.Label>
+                                    <Form.Control type="text"
                                         placeholder="Name to Update"
                                         value={inputs.data[index].name}
                                         onChange={event=>setInfoCardName(index,event.target.value)}
                                     />
                                     {stuff.team.map((information,index2)=>(
                                         <div>
-                                        <h4>Person {index2+1} of Section {index+1}</h4>
-                                        <input type="text"
+                                        <Form.Label>Person {index2+1} of Section {index+1}</Form.Label>
+                                        <Form.Control type="text"
                                         placeholder="Name to Update"
                                         value={information.personName}
                                         onChange={event=>setInfoCardPerson(index,index2,event.target.value)}
                                         required/>
-                                        <h4>Information of Person {index2+1} Section {index+1}</h4>
-                                        <textarea type="text"
+                                        <Form.Label>Information of Person {index2+1} Section {index+1}</Form.Label>
+                                        <Form.Control as="textarea" type="text"
                                         placeholder="Information to Update"
                                         value={information.info}
                                         onChange={event=>setInfoCardText(index,index2,event.target.value)}
                                         required/>
-                                        <button type="submit" onClick={handleSubmit}> Update Employee</button>
-                                        <button type="submit" onClick={event=>deleteEmployee(index2,event)}>Delete Employee</button>
+                                        <Button type="submit" onClick={handleSubmit}> Update Employee</Button>
+                                        <Button type="submit" onClick={event=>deleteEmployee(index2,event)}>Delete Employee</Button>
                                         </div>
-                                    )
-                                        
-                                    )}
-                                </form>
+                                    ))}
+                                </Form>
                             </div>
                         )
                     }
                 })
             }
             <h2>Add new section</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Section Title:</label><p></p>
-                <input type="text" placeholder="Name for New Section" name="name" value={section.name} onChange={handleNewSectionChange} required></input>
-                <p></p><label>Section Information:</label><p></p>
-                <textarea type="text" placeholder="Text for New Section" name="text" value={section.text} onChange={handleNewSectionChange} required></textarea>
-                <p></p><label>Section Unique id:</label><p></p>
-                <input type="text" placeholder="ID for New Section" name="id" value={section.id} onChange={handleNewSectionChange} required></input>
-                <button type="submit" onClick={handleNewSection}>Make new Section</button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Section Title:</Form.Label>
+                <Form.Control type="text" placeholder="Name for New Section" name="name" value={section.name} onChange={handleNewSectionChange} required></Form.Control>
+                <Form.Label>Section Information:</Form.Label>
+                <Form.Control as="textarea" type="text" placeholder="Text for New Section" name="text" value={section.text} onChange={handleNewSectionChange} required></Form.Control>
+                <Form.Label>Section Unique id:</Form.Label>
+                <Form.Control type="text" placeholder="ID for New Section" name="id" value={section.id} onChange={handleNewSectionChange} required></Form.Control>
+                <Button type="submit" onClick={handleNewSection}>Make new Section</Button>
+            </Form>
             <h2>Add new employee</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Employee information:</label>
-                <input type="text" placeholder="Name for New employee" name="personName" value={employee.personName} onChange={handleEmployeeChange} required></input>
-                <p>Employee information:</p>
-                <textarea type="text" placeholder="Text for New employee" name="info" value={employee.info} onChange={handleEmployeeChange} required></textarea>
-                <button type="submit" onClick={event=>handleNewEmployee(event)}> Make new Employee</button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Employee information:</Form.Label>
+                <Form.Control type="text" placeholder="Name for New employee" name="personName" value={employee.personName} onChange={handleEmployeeChange} required></Form.Control>
+                <Form.Label>Employee information:</Form.Label>
+                <Form.Control as="textarea" type="text" placeholder="Text for New employee" name="info" value={employee.info} onChange={handleEmployeeChange} required></Form.Control>
+                <Button type="submit" onClick={event=>handleNewEmployee(event)}> Make new Employee</Button>
+            </Form>
         </div>
     );
 }
--- client/src/components/Product/UpdateProductForm.js
+++ client/src/components/Product/UpdateProductForm.js
@@ -1,64 +1,68 @@
 import React,{useState} from 'react';
 import useProductPage from './useProductPage';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 const ProductUpdate=()=>{
     let {inputs,handleSubmit,setText,setName,handleNewSection,
         handleNewSectionChange,section,deleteSection}=useProductPage();
     return(
         <div>
+            <h2>Update or Delete Sections on Product Page</h2>
             {
                 inputs.data.map((stuff,index)=>{
                     if(stuff.type==="info"){
                         return(
-                            <form>
-                                <h4>Title of Section {index+1}:</h4>
-                                <input type="text"
+                            <Form>
+                                <Form.Label>Title of Section {index+1}:</Form.Label>
+                                <Form.Control type="text"
                                 placeholder="Name to Update"
                                 value={inputs.data[index].name}
                                 onChange={event=>setName(index,event.target.value)}
                                 required/>
-                                <h4>Text of Section {index+1}</h4>
-                                <textarea type="text"
+                                <Form.Label>Text of Section {index+1}</Form.Label>
+                                <Form.Control as="textarea" type="text"
                                 placeholder="Text to Update"
                                 value={inputs.data[index].text}
                                 onChange={event=>setText(index,event.target.value)}
                                 required/>
-                                <button type="submit" onClick={handleSubmit}> Update Section {index+1}</button>
-                                <button type="submit" onClick={event=>deleteSection(index,event)}>Delete Section {index+1}</button>
-                            </form>
+                                <Button type="submit" onClick={handleSubmit}> Update Section {index+1}</Button>
+                                <Button type="submit" onClick={event=>deleteSection(index,event)}>Delete Section {index+1}</Button>
+                            </Form>
                         )
                     }
                     else{
                         return(
                             <div>
-                                <form onSubmit={handleSubmit}>
-                                    <h4>Title of Section {index+1}:</h4>
-                                    <input type="text"
+                                <h2>Update Order Link</h2>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Label>Title of Section {index+1}:</Form.Label>
+                                    <Form.Control type="text"
                                         placeholder="Name to Update"
                                         value={stuff.name}
                                         onChange={event=>setName(index,event.target.value)}
                                     />
-                                    <h4>Input the Link</h4>
-                                    <input type="text"
+                                    <Form.Label>Input the Link</Form.Label>
+                                    <Form.Control type="text"
                                         placeholder="Link to Update"
                                         value={stuff.text}
                                         onChange={event=>setText(index,event.target.value)}
                                     />
-                                </form>
+                                </Form>
                             </div>
                         )
                     }
                 })
             }
             <h2>Add new section</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Section Title:</label><p></p>
-                <input type="text" placeholder="Name for New Section" name="name" value={section.name} onChange={handleNewSectionChange} required></input>
-                <p></p><label>Section Information:</label><p></p>
-                <textarea type="text" placeholder="Text for New Section" name="text" value={section.text} onChange={handleNewSectionChange} required></textarea>
-                <p></p><label>Section Unique id:</label><p></p>
-                <input type="text" placeholder="ID for New Section" name="id" value={section.id} onChange={handleNewSectionChange} required></input>
-                <button type="submit" onClick={handleNewSection}>Make new Section</button>
-            </form>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Section Title:</Form.Label>
+                <Form.Control type="text" placeholder="Name for New Section" name="name" value={section.name} onChange={handleNewSectionChange} required></Form.Control>
+                <Form.Label>Section Information:</Form.Label>
+                <Form.Control as="textarea" type="text" placeholder="Text for New Section" name="text" value={section.text} onChange={handleNewSectionChange} required></Form.Control>
+                <Form.Label>Section Unique id:</Form.Label>
+                <Form.Control type="text" placeholder="ID for New Section" name="id" value={section.id} onChange={handleNewSectionChange} required></Form.Control>
+                <Button type="submit" onClick={handleNewSection}>Make new Section</Button>
+            </Form>
         </div>
     );
 }
