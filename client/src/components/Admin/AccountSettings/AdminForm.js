3a1ebaf01b22a9976fece2c7a5c90011af4ee5e1
Jon <jconlin@ufl.edu>
2019-11-30 22:35:07 -0500

    user modification complete

--- client/src/components/Admin/AccountSettings/AdminForm.js
+++ client/src/components/Admin/AccountSettings/AdminForm.js
@@ -2,6 +2,13 @@ import React, {useState, useEffect, memo} from 'react';
 import axios from 'axios';
 import {Button, Modal, Form} from 'react-bootstrap';
 
+const permissions = [
+    {'super': 'Has permission to edit anything from user accounts to the website itself.'},
+    {'create': 'Permission to allow an admin to create new users.'},
+    {'user-edit': 'Permission to allow an admin to edit users.'},
+    {'page-edit': 'Permission to allow an admin to edit text and images on pages.'},
+];
+
 const AdminForm = () => {
 
     const [users, setUsers] = useState([]);
@@ -10,7 +17,7 @@ const AdminForm = () => {
 
     useEffect(() => {
         updateUsers();
-    });
+    }, []);
 
     const updateUsers = () => {
         axios.get('/api/admin/user', {
@@ -18,6 +25,7 @@ const AdminForm = () => {
                 email: 'FINDALL'
             }
         }).then((res) => {
+            console.log(res.data);
             setUsers(res.data);
         });
     };
@@ -34,35 +42,50 @@ const AdminForm = () => {
             } else
                 return true;
         }).map(user => {
-            return <UserRow user={user} setCardState={setCardState} cardState={cardState}/>;
+            return <UserRow user={user} setCardState={setCardState} cardState={cardState} update={updateUsers}/>;
         });
 
-        return <table>{userList}</table>
+        return <div>{userList}</div>
     };
 
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label column=''>Email</Form.Label>
-                <Form.Control type="text" placeholder="Search users by email..." value={searchFilter} onChange={e => setSearchFilter(e.target.value)}/>
-                {getUsers}
-            </Form.Group>
-        </Form>
+        <div className='border-primary border p-4 m-3'>
+            <Form>
+                <Form.Group>
+                    <Form.Label column=''><h2>Modify Users</h2></Form.Label>
+                    <Form.Control type="text" placeholder="Search users by name and modify their information..."
+                                  value={searchFilter}
+                                  onChange={e => setSearchFilter(e.target.value)}/>
+                </Form.Group>
+            </Form>
+            <table className='table table-striped table-hover'>
+                {getUsers()}
+            </table>
+        </div>
     );
 };
 
 const UserRow = memo((props) => {
     const handleClose = () => props.setCardState(false);
     const handleShow = () => props.setCardState(true);
+    const update = () => props.update();
 
     return (
         <div key={props.user.id}>
-            <tr key={props.user.email} onClick={handleShow}>
+            <tr>
+                <td>
+                    <b>Email</b>
+                </td>
+                <td>
+                    <b>Name</b>
+                </td>
+            </tr>
+            <tr key={props.user.email} onClick={handleShow} style={{cursor: 'pointer'}}>
                 <td>{props.user.email}</td>
                 <td>{props.user.name}</td>
             </tr>
             {props.cardState ? <ProfileCard name={props.user.name} email={props.user.email} handleClose={handleClose}
-                                            show={props.cardState}/> : null}
+                                            show={props.cardState} update={update}/> : null}
         </div>
     );
 });
@@ -71,18 +94,79 @@ const ProfileCard = (props) => {
 
     const handleClose = props.handleClose;
     const show = props.show;
+    const handleSubmit = () => {
+        const data = {
+            newName: name,
+            email: props.email,
+            newEmail: email,
+            password: password,
+            password1: password1,
+        };
+        axios.post('/api/admin/modify', data).then(res => {
+            if (res.status === 200)
+                setError('');
+        }).catch(err => setError('error')).finally(() => {
+            setSubmitted(true);
+            props.update();
+        });
+    };
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [error, setError] = useState('');
+    const [submitted, setSubmitted] = useState(false);
 
     return <>
-        <Modal show={show} onHide={handleClose}>
+        <Modal aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title>{props.name}'s Profile</Modal.Title>
             </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Body>
+                {submitted && error === '' ?
+                    <span>Successfully modified {props.name}'s information!</span> : (submitted && error !== '' ?
+                        <span>There was an error updating this user's profile.</span> : null)}
+                <div className='p-2 border-primary border-bottom'>
+                    Current name: {props.name}
+                    <br/>
+                    Current email: {props.email}
+                </div>
+                <br/>
+                <Form>
+                    <Form.Group>
+                        <Form.Label column=''><strong>Modify Name</strong></Form.Label>
+                        <Form.Control type="text" placeholder="Enter a new name..."
+                                      value={name}
+                                      onChange={e => setName(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label column=''><strong>Modify Email</strong></Form.Label>
+                        <Form.Control type="email" placeholder="Enter a new email..."
+                                      value={email}
+                                      onChange={e => setEmail(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label column=''><strong>Modify Password</strong></Form.Label>
+                        <Form.Control type="text" placeholder="Enter a new password..."
+                                      value={password}
+                                      onChange={e => setPassword(e.target.value)}/>
+                        <Form.Control className='mt-2' type="text" placeholder="Confirm new password..."
+                                      value={password1}
+                                      onChange={e => setPassword1(e.target.value)}/>
+                    </Form.Group>
+                </Form>
+                {
+                    // TODO: allow modifiable permissions and create optional select buttons
+                    permissions.map(permission => {
+                    })
+                }
+            </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" onClick={handleSubmit}>
                     Save Changes
                 </Button>
             </Modal.Footer>
