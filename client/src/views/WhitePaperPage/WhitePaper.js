62878536705d0430c13a67a8cb17decc94387639
Nicholas Alvarez <nick12al@gmail.com>
2019-12-02 11:00:36 -0500

    added navbar to whitepages

--- client/src/views/WhitePaperPage/WhitePaper.js
+++ client/src/views/WhitePaperPage/WhitePaper.js
@@ -21,10 +21,6 @@ const WhitePaper = () => {
         <div className="main">
             <h1> Documents </h1>
             <NavBar items={navItems["home-bar"]}/>
-            <a className="navbar-brand nav-item sticky-left" href="/home">
-                <img src="/assets/home.png" width="30" height="30" alt=""/>
-            </a>
-
             {pdfViewers}
         </div>
     )
